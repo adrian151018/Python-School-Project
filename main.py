41d35e3db8fbfa7e1076e2719e77a88799095499
torbalanQdeDeca <106234002+torbalanQdeDeca@users.noreply.github.com>
2023-05-17 15:49:40 +0300

    basics

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import pygame
 
 WIDTH, HEIGHT = 1000, 500
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+FPS = 60
 
 def main():
+    clock = pygame.time.Clock()
     running = True
     while(running):
-        
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
