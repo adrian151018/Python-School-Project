80a06259e84f7c9e0c5ff328af8e25372542c01d
torbalanQdeDeca <106234002+torbalanQdeDeca@users.noreply.github.com>
2023-05-31 19:00:36 +0300

    Scrolling background

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import pygame
+import math
 
 WIDTH, HEIGHT = 1000, 500
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+background = pygame.image.load("jungle.png").convert_alpha()
+background_width = background.get_width()
+tiles = math.ceil(WIDTH / background_width + 1)
+SCROLL = 0
 
-def main():
-    clock = pygame.time.Clock()
-    running = True
-    while(running):
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
 
-    pygame.quit()
 
-if __name__ == "__main__":
-    main()
+clock = pygame.time.Clock()
+running = True
+while(running):
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    
+    for i in range(0, tiles):
+        window.blit(background, (i * background_width + SCROLL, 0))
+
+    SCROLL -= 5
+
+    if abs(SCROLL) > background_width:
+        SCROLL = 0
+
+    pygame.display.update()
+
+pygame.quit()
