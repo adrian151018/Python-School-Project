3b46fef49ad6444ab38f38a5b9a7c89a376b8d7e
torbalanQdeDeca <106234002+torbalanQdeDeca@users.noreply.github.com>
2023-06-07 02:18:58 +0300

    Menu and auto play

--- functions.py
+++ functions.py
@@ -1,21 +1,5 @@
 import pygame
 
-def game_over(screen, font, score):
-    game_over_surface = pygame.Surface((600, 300))
-    game_over_surface.fill((255, 0, 0))  # Red color for the game over rectangle
-
-    game_over_text = font.render("Game Over", True, (255, 255, 255))  # White color for the text
-    game_over_text_rect = game_over_text.get_rect(center = (game_over_surface.get_width() // 2, game_over_surface.get_height() // 2 - 50))
-
-    score_text = font.render("Score: " + str(score), True, (255, 255, 255))
-    score_text_rect = score_text.get_rect(center = (game_over_surface.get_width() // 2, game_over_surface.get_height() // 2 + 50))
-
-    game_over_surface.blit(game_over_text, game_over_text_rect)
-    game_over_surface.blit(score_text, score_text_rect)
-
-    screen.blit(game_over_surface, (200, 100))
-    pygame.display.update()
-
 def get_score(window, font, score):
     score_img = font.render("Score: " + str(score), True, (0, 0, 0))
     window.blit(score_img, (0, 0))
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import math
 from obstacles import Obstacle
 from character import Bike
-from functions import game_over, get_score
+from functions import get_score
 
 pygame.init()
 
@@ -16,19 +16,21 @@ font = pygame.font.SysFont("comicsansms", 35)
 background = pygame.image.load("mountains.png").convert_alpha()
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 background_width = background.get_width()
-tiles = math.ceil(WIDTH / background_width + 2)
+tiles = math.ceil(WIDTH / background_width + 1)
 SCROLL = 0
 
 # setting up clock and FPS
 FPS = 60
 clock = pygame.time.Clock()
+endprogram = False
 
 # making a Bike object which would be our "character"
 movingbike = pygame.sprite.GroupSingle()
 bike = Bike(30, 275)
 movingbike.add(bike)
-moving = False
 jumping = False
+moving = True
+deaths = False
 GRAVITY = 1
 JUMP_HEIGHT = 20
 JUMP_SPEED = JUMP_HEIGHT
@@ -40,46 +42,70 @@ log = Obstacle("log.png",  700, 330)
 log2 = Obstacle("log.png", 1700, 330)
 obstacles.add(log)
 obstacles.add(log2)
+trigger_zone_x = log.rect.x
+trigger_zone_y = log.rect.y
+trigger_zone_width = log.rect.width
+trigger_zone_height = log.rect.height + 200
 
-# main loop
+def menu():
+    global endprogram
+    endprogram = False
+    menurun = True
+    window.fill((255, 255, 255))
+    while menurun:
+        if deaths:
+            menu_text = font.render("Press any key to restart", True, (0, 0, 0))  # White color for the text
+            score_text = font.render("Your Score: " + str(score), True, (0, 0, 0))
+            score_text_rect = score_text.get_rect(center = (WIDTH // 2, HEIGHT // 2 + 50))
+            window.blit(score_text, score_text_rect)
+        else:
+            menu_text = font.render("Press any key to start", True, (0, 0, 0))
+
+        menu_text_rect = menu_text.get_rect(center = (WIDTH // 2, HEIGHT // 2 - 50))
+        window.blit(menu_text, menu_text_rect)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menurun = False
+                endprogram = True
+            if event.type == pygame.KEYDOWN:
+                menurun = False
+
+menu()
+
+
 running = True
 enabled = True
-while running:
+while running and not endprogram:# main loop
     clock.tick(FPS)
-    # event handler
-    for event in pygame.event.get():
+    for event in pygame.event.get():# event handler
         if event.type == pygame.QUIT:
             running = False
         if enabled:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    moving = True
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    moving = False
             if event.type == pygame.KEYDOWN and not jumping:
                 if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
                     jumping = True
 
-    # Drawing the background, obstacles and bike.
-    for i in range(0, tiles):
+    for i in range(0, tiles):# Drawing the background, obstacles and bike.
         window.blit(background, (i * background_width + SCROLL, 0))
         obstacles.draw(window)
         log.update_pos(700 + SCROLL)
         log2.update_pos(1700 + SCROLL)
+        trigger_zone_x += SCROLL
     movingbike.draw(window)
     get_score(window, font, score)
 
-    # Checking for collision between the bike's right side and the log's left side
-    if bike.rect.colliderect(log.rect):
-        game_over(window, font, score)
+    if bike.rect.colliderect(log.rect):# Checking for collision between the bike's right side and the log's left side
+        deaths = True
         enabled = False
         moving = False
-        jumping = False
-
-    # Checking for pressed arrow key and changing the
-    # variables responsible for the movement(scrolling effect)
-    # of the background, objects and bike
+        menu()
+        score = 0
+        enabled = True
+        moving = True
+        SCROLL = 0
+        continue
     if moving:
         draw_background = True
         SCROLL -= 9
@@ -87,16 +113,14 @@ while running:
             SCROLL = 0
         movingbike.update(0.3)
 
-    # Checking for pressed space or arrow up
-    # and starting jumping mechanics
-    if jumping:
+    if jumping:# Checking for pressed space or arrow up and starting jumping mechanics
         bike.rect.y -= JUMP_SPEED
         JUMP_SPEED -= GRAVITY
         if JUMP_SPEED < -JUMP_HEIGHT:
             jumping = False
             JUMP_SPEED = JUMP_HEIGHT
-            score += 1
+            if bike.rect.colliderect((trigger_zone_x, trigger_zone_y, trigger_zone_width, trigger_zone_height)):
+                score += 1
 
-     # Updating the display
-    pygame.display.update()
+    pygame.display.update()# Updating the display
 pygame.quit()
